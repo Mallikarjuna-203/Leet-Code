c70b99bf87ee5169c4f7ac6b56402e1d52ede04f
YERRAGUDLA MALLIKARJUNA <yerragudlamallikarjuna143@gmail.com>
2026-09-24 12:03:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.c
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -1,21 +1,19 @@
 bool isAnagram(char* s, char* t)
 {
-    int str[26] = {0};
-
-    if (strlen(s) != strlen(t))
-        return false;
-
-    for (int i = 0; s[i] != '\0'; i++)
-    {
-        str[s[i] - 'a']++;
-        str[t[i] - 'a']--;
-    }
-
-    for (int i = 0; i < 26; i++)
-    {
-        if (str[i] != 0)
-            return false;
-    }
-
-    return true;
+  int str[26]={0};
+  if(strlen(s)!=strlen(t))
+  {
+    return false;
+  }
+  for(int i=0;i<strlen(s);i++)
+  {
+    str[s[i]-'a']++;
+    str[t[i]-'a']--;
+  }
+  for(int i=0;i<26;i++)
+  {
+    if(str[i]!=0)
+    return false;
+  }
+  return true;
 }
