565650d8595527d5cfa59cd2598540761abf16b5
YERRAGUDLA MALLIKARJUNA <yerragudlamallikarjuna143@gmail.com>
2026-09-24 11:37:55 +0530

    Time: 0 ms (100%), Space: 20.3 MB (83.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,13 +1,13 @@
 void moveZeroes(int* arr, int numsSize) {
-    int j=0;
-    for(int i=0;i<numsSize;i++)
+   int k=0;
+   for(int i=0;i<numsSize;i++)
+   {
+    if(arr[i]!=0)
     {
-        if(arr[i]!=0)
-        {
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            j++;
-        }
+        int temp=arr[i];
+        arr[i]=arr[k];
+        arr[k]=temp;
+        k++;
     }
+   }
 }
